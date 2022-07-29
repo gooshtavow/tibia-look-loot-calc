b8f6eff4b007e30179281a42618d1a9467159f56
Gustavo Santos <gu_fs@hotmail.com>
2022-07-29 00:23:51 -0300

    Add the first working version

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# tibia-look-loot-calc
-Python3 look based loot calculator for Tibia.
+# Tibia Look Loot Calculator
+
+This is yet another loot calculator for the Tibia MMORPG. If you've ever used Tibia Wiki's loot calculator, this is pretty much like that one, except way simpler. It works by reading the text you get from looking at items, parsing each item and quantity, and then summing it all up in the end.
+
+There are probably some corner cases I haven't considered, so take the results with a grain of salt. Feel free to report bugs.
+
+## Usage
+
+ 1. On the Tibia client, clear your server log channel and look at every item you want to sell to NPCs. This includes looking at ALL items you want to sell, even if they're duplicates.
+ 2. Copy everything in your server log and paste it into a text file called "paste.txt". Place this text file in the same directory as the `lootcalc.py` script. An example file is provided.
+ 3. Run the `lootcalc.py` script. It will print out the total gold value of the items you looked at.
+
+## Important Notes
+
+ - The `sorted_items_and_values.csv` file includes each item's name and their NPC value in the `singular_name,plural_name,weight,value` format. It was built using the [tibiawiki-sql](https://github.com/Galarzaa90/tibiawiki-sql) project.
+ - This calculator has not yet been updated with the new look message format, which includes the quantity of every item, like "10 ham" or "16 meat", but it still works regardless.
+
+## Future Features
+
+The calculator is still very simple. It only reads your logs and spits out the total value. Someday, it might:
+
+ - Also print the amount of each item and their value;
+ - Show which NPC buys each item;
+ - Have a simple GUI;
+ - ... more? Who knows!
---
+++ lootcalc.py
@@ -0,0 +1,104 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+# Feel free to contact me at GitHub
+
+import math
+import re
+
+item_data = []
+item_names = set()
+
+def load_csv_into_memory():
+    with open ("./sorted_items_and_values.csv", "r") as f:
+        for line in f:
+            line = line[:-1].split(",")
+            object = {
+                "name": line[0],
+                "plural": line[1],
+                "weight": float(line[2]),
+                "value": int(line[3])
+            }
+            item_data.append(object)
+            item_names.add(line[0])
+            item_names.add(line[1])
+
+
+def parse_input(file):
+    with open(f"{file}", "r") as f:
+        itemlist = []
+        parsed_item = []
+        for line in f:
+            item = re.findall(r"You see (\d*)[\s]*([^.\(]+)", line)
+            if item:
+                item = list(item[0])
+                # Now, item[0] holds the amount and item[1] holds item name
+
+                # Remove "a", "an" and whitespaces
+                if item[0] == "" and item[1][0] == "a":
+                    if item[1][1:3] == "n ":
+                        item[1] = item[1][3:]
+                    elif item[1][1] == " ":
+                        item[1] = item[1][2:]
+                if item[1][-1] == " ":
+                    item[1] = item[1][:-1]
+
+                # Remove "that..." for rings
+                if "that is" in item[1] or "that will" in item[1]:
+                    aux = ""
+                    item[1] = item[1].split(" ")
+                    for word in item[1]:
+                        if word == "that":
+                            break
+                        else:
+                            aux = aux + word + " "
+                    item[1] = aux[:-1]
+                if item[0] == "":
+                    parsed_item.append(item[0])
+                else:
+                    parsed_item.append(int(item[0])) # amount
+                parsed_item.append(item[1]) # name
+
+            weight = re.findall(r"weighs? (\d+.\d\d)", line)
+            if weight:
+                parsed_item.append(float(weight[0])) # weight
+                itemlist.append(parsed_item)
+                parsed_item = []
+
+    return itemlist
+
+
+def calculate_value(item_list):
+    # item[0] = amount
+    # item[1] = name
+    # item[2] = weight
+    total_value = 0
+    for item in item_list:
+        if item[1] in item_names:
+            singular_item = next((it for it in item_data if it["name"] == item[1]), False)
+            if singular_item:
+                if math.isclose(item[2], singular_item["weight"], abs_tol=0.001):
+                    total_value += singular_item["value"]
+                else:
+                    total_value += (item[2]/singular_item["weight"]) * singular_item["value"]
+            else:
+                plural_item = next((it for it in item_data if (it["plural"] == item[1]) and math.isclose(item[2], item[0] * it["weight"], abs_tol=0.001)), False)
+                if plural_item:
+                    total_value += plural_item["value"] * item[0]
+    return int(total_value)
+
+
+load_csv_into_memory()
+item_list = parse_input("paste.txt")
+loot_value = calculate_value(item_list)
+print(loot_value)
